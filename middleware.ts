f00df996ca7417f4fe012913f4d5800525748b33
Enrique Palomino <octepc.97@gmail.com>
2024-03-23 11:25:39 -0500

    ADD initial config like fonts and images

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+
+export default authMiddleware({
+  publicRoutes:[
+    '/'
+  ]
+});
+
+export const config = {
+  matcher: [
+
+
+    "/((?!.+\\.[\\w]+$|_next).*)",
+    // Re-include any files in the api or trpc folders that might have an extension
+    "/(api|trpc)(.*)"
+  ]
+};
